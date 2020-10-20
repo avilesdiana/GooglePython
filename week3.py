4e52e1ffbfc7ed268bc9adcc1283eb5f7bce1bfd
avilesdiana <avilesdiana@outlook.com>
2020-10-19 23:32:16 -0500

    Final Loops

--- week3.py
+++ week3.py
@@ -237,3 +237,182 @@ for n in range(0,10):
     print(n, factorial(n+1))
 
 """
+#Factorial with Recursion
+"""
+recursion structure
+def recursive_function(parameters):
+    if base_case_condition(parameters):
+        return base_case_value
+    recursive_function(modified_parameters)
+"""
+"""
+def sum_positive_numbers(n):
+    # The base case is n being smaller than 1
+    if n < 1:
+        return 0
+
+    # The recursive case is adding this number to 
+    # the sum of the numbers smaller than this one.
+    return  n + sum_positive_numbers(n-1)
+
+print(sum_positive_numbers(3)) # Should be 6
+print(sum_positive_numbers(5)) # Should be 15
+"""
+
+#Quiz Recursion
+"""
+Fill in the blanks to make the is_power_of function return whether the number is a
+ power of the given base. Note: base is assumed to be a positive number. Tip: 
+ for functions that return a boolean value, you can return the result of a comparison. """
+
+"""
+def is_power_of(number, base):
+  # Base case: when number is smaller than base.
+  if number ==1:
+    return True
+  if number < base:
+    # If number is equal to 1, it's a power (base**0).
+    return False
+  number /= base
+
+  # Recursive case: keep dividing number by base.
+  return is_power_of(number, base)
+
+print(is_power_of(8,2)) # Should be True
+print(is_power_of(64,4)) # Should be True
+print(is_power_of(70,10)) # Should be False
+
+"""
+"""
+The count_users function recursively counts the amount of users that belong to a group 
+in the company system, by going through each of the members of a group and if one of them 
+is a group, recursively calling the function and counting the members. But it has a bug! 
+Can you spot the problem and fix it?
+"""
+
+"""
+def count_users(group):
+  count = 0
+  for member in get_members(group):
+    count += 1
+    if is_group(member):
+      count -=1
+      count += count_users(member)
+  return count
+
+print(count_users("sales")) # Should be 3
+print(count_users("engineering")) # Should be 8
+print(count_users("everyone")) # Should be 18
+"""
+
+"""
+Implement the sum_positive_numbers function, as a recursive function that returns the 
+sum of all positive numbers between the number n received and 1. For example, when n 
+is 3 it should return 1+2+3=6, and when n is 5 it should return 1+2+3+4+5=15.
+"""
+"""
+def sum_positive_numbers(n):
+   
+  if n < 1: 
+    return n
+  return n+ sum_positive_numbers(n-1)
+
+print(sum_positive_numbers(3)) # Should be 6
+print(sum_positive_numbers(5)) # Should be 15
+"""
+
+"""
+Complete the function digits(n) that returns how many digits the number has. 
+For example: 25 has 2 digits and 144 has 3 digits. Tip: you can figure out the digits of
+ a number by dividing it by 10 once per digit until there are no digits left. """
+
+"""
+def digits(n):
+	count = 0
+	if n == 0:
+	  count = 1
+	while (n != 0 ):
+		count += 1
+		n//=10
+	return count
+	
+print(digits(25))   # Should print 2
+print(digits(144))  # Should print 3
+print(digits(1000)) # Should print 4
+print(digits(0))    # Should print 1
+print(digits(123456789)) #Should print 9
+"""
+"""
+This function prints out a multiplication table (where each number is the 
+result of multiplying the first number of its row by the number at the top
+ of its column). Fill in the blanks so that calling multiplication_table(1, 3) 
+ will print out:
+
+1 2 3
+
+2 4 6
+
+3 6 9
+
+"""
+"""
+def multiplication_table(start, stop):
+	for x in range(start,stop+1):
+		for y in range(start,stop +1):
+			print(str(x*y), end=" ")
+		print()
+
+multiplication_table(1, 3)
+# Should print the multiplication table shown above
+"""
+
+
+"""Pregunta 5
+
+The counter function counts down from start to stop when start is bigger than stop, 
+and counts up from start to stop otherwise. Fill in the blanks to make this work correctly. """
+
+"""
+def counter(start, stop):
+	x = start
+	if x > stop:
+		return_string = "Counting down: "
+		while x >= stop:
+			return_string += str(x)
+			if x > stop:
+				return_string += ","
+			x= x-1
+	else:
+		return_string = "Counting up: "
+		while x <= stop:
+			return_string += str(x)
+			if x< stop:
+				return_string += ","
+			x = x+1
+	return return_string
+
+print(counter(1, 10)) # Should be "Counting up: 1,2,3,4,5,6,7,8,9,10"
+print(counter(2, 1)) # Should be "Counting down: 2,1"
+print(counter(5, 5)) # Should be "Counting up: 5"
+
+"""
+
+"""
+The even_numbers function returns a space-separated string of all positive numbers that are 
+divisible by 2, up to and including the maximum that's passed into the function. For example, 
+even_numbers(6) returns “2 4 6”. Fill in the blank to make this work. """
+
+"""
+def even_numbers(maximum):
+	return_string = ""
+	for x in range(2,maximum+1):
+		if(x%2 ==0):
+			return_string += str(x) + " "
+	return return_string.strip()
+
+print(even_numbers(6))  # Should be 2 4 6
+print(even_numbers(10)) # Should be 2 4 6 8 10
+print(even_numbers(1))  # No numbers displayed
+print(even_numbers(3))  # Should be 2
+print(even_numbers(0))  # No numbers displayed
+"""
