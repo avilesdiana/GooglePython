09b46d66dc3ed98ce02914a4ddc38f918878cd65
avilesdiana <avilesdiana@outlook.com>
2020-10-19 19:03:49 -0500

    Loops Chear Sheet

--- week3.py
+++ week3.py
@@ -204,3 +204,16 @@ for x in range(0,101,10):
 """
 
 #Nested for loops
+
+"""
+for left in range(7):
+    for right in range(left,7):
+        print("[" + str(left)+ " ¨ " + str(right) + "]", end =" ")
+    print()
+"""
+
+teams = [ 'Dragons', 'Wolves', 'Pandas', 'Unicorns']
+for home_team in teams:
+    for away_team in teams:
+       if home_team != away_team:
+           print(home_team + " vs " + away_team)
