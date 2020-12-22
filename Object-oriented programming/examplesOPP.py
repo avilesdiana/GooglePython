70fe1537faa618084566a0bbf8fb3f39d886d041
avilesdiana <avilesdiana@outlook.com>
2020-12-21 22:44:48 -0600

    Constructor

--- Object-oriented programming/examplesOPP.py
+++ Object-oriented programming/examplesOPP.py
@@ -1,3 +1,5 @@
+#Instance
+
 """
 class Piglet:
     name = "piglet"
@@ -22,3 +24,46 @@ print(diana.pig_years())
 
 """
 
+#Constructor
+"""
+class Apple:
+    def __init__(self,color, flavor):
+        self.color = color
+        self.flavor = flavor
+
+diana = Apple("red", "sweet")
+print(diana.color)
+"""
+"""
+In this code, there's a Person class that has an attribute name, 
+which gets set when constructing the object. Fill in the blanks so that 
+1) when an instance of the class is created, the attribute gets set correctly, and 
+2) when the greeting() method is called, the greeting states the assigned name."""
+
+"""
+class Person:
+    def __init__(self, name):
+        self.name = name
+    def greeting(self):
+        # Should return "hi, my name is " followed by the name of the Person.
+        return "hi, my name is {}".format(self.name) 
+
+# Create a new instance with a name of your choice
+some_person = Person("diana")  
+# Call the greeting method
+print(some_person.greeting())
+
+"""
+
+"""
+class Apple:
+    def __init__(self, color, flavor):
+        self.color = color
+        self.flavor = flavor
+    
+    def __str__(self):
+        return "This apple is {} and it's flavor is {}". format(self.color, self. flavor)
+
+diana = Apple("red", "Sweet")
+print(diana)
+"""
