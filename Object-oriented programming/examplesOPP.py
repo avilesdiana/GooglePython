4a203a4ae1378da14e33d85917206ba689e2c55f
avilesdiana <avilesdiana@outlook.com>
2020-12-21 22:20:15 -0600

    instance

---
+++ Object-oriented programming/examplesOPP.py
@@ -0,0 +1,24 @@
+"""
+class Piglet:
+    name = "piglet"
+    def speak(self):
+        print("Oik! I'm {}! Oink!".format(self.name))
+
+hamlet = Piglet()
+hamlet.name = "Hamlet"
+print(hamlet.speak())
+
+"""
+"""
+class Piglet:
+    years = 0
+    def pig_years(self):
+        return self.years * 18
+
+
+diana = Piglet()
+diana.years = 15
+print(diana.pig_years())
+
+"""
+
